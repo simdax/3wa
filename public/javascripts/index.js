5791ee617132501a7bdaa6323f1b3d5fc8f5748f
simon <simoncornaz@gmail.com>
2017-04-26 17:27:59 +0200

    disparition last logo

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,11 +3,17 @@ $(function () {
   $('.badge').lettering();
 
   $(window).scroll(function () {
-    if ($(window).scrollTop() > 2000){
-      $('#background1').css("background-position","75% 25%");
+    if ($(window).scrollTop() > 2000 & $(window).scrollTop() < 2100){
+      $('#background1').css("background-position","77% 25%");
+      $('#background1').css("background-size","13%");
+    }
+    else if($(window).scrollTop() > 2100){
+      console.log('io');
+      $('#background1').css("background-size","0");
     }
     else{
-      $('#background1').css("background-position","20% 25%");
+      $('#background1').css("background-size","13%");
+      $('#background1').css("background-position","21% 25%");
     }
 
   })
